77ce9a2f212e4fb8ed01b45fa194e88ecd91d64a
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2020-05-22 01:22:46 -0500

    add ffmpeg binaries

---
+++ scripts/notarize.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    appBundleId: process.env.BUNDLE_ID,
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS,
+    ascProvider: '3M3M35BZPF'
+  });
+};
